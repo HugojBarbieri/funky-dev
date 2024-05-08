14f07ebe3ca12adcc39a1615e5aacacbed09c0fe
Javier <hugo.barbieri.ar@gmail.com>
2024-05-08 16:31:11 -0300

    another push

---
+++ package-service/src/main/java/com/funky/packageservice/model/CustomerDTO.java
@@ -0,0 +1,16 @@
+package com.funky.packageservice.model;
+
+public class CustomerDTO {
+    private String name;
+
+    public CustomerDTO() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
